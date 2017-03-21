ca23548074dd23ae4e9148cc8e37e32099638094
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-03-21 13:08:16 -0700

    add buttons to change page theme from light to dark

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,39 @@
 
 $(function() {
-  $("p").click(function() {
-    $("img").show();
+
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-theme");
   });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-theme");
+  });
+
+/* keeping the button reference section see HTML, keeping...
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $(".clickable").click(function() {
+    $("#showing").fadeToggle();
+    $("#hidden").fadeToggle();
+  });
+*/
+
+
+
+
 });
